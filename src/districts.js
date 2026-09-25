7b79bfa1722024921b572804e27db64a4cd30643
thorughbred78ai-svg <thorughbred78ai@gmail.com>
2026-09-25 09:02:24 +0800

    Create districts.js

---
+++ src/districts.js
@@ -0,0 +1,57 @@
+export const TAOYUAN_DISTRICTS = [
+  {
+    name: "桃園區",
+    locationId: "68000010"
+  },
+  {
+    name: "中壢區",
+    locationId: "68000020"
+  },
+  {
+    name: "平鎮區",
+    locationId: "68000030"
+  },
+  {
+    name: "八德區",
+    locationId: "68000040"
+  },
+  {
+    name: "楊梅區",
+    locationId: "68000050"
+  },
+  {
+    name: "蘆竹區",
+    locationId: "68000060"
+  },
+  {
+    name: "大溪區",
+    locationId: "68000070"
+  },
+  {
+    name: "龜山區",
+    locationId: "68000080"
+  },
+  {
+    name: "大園區",
+    locationId: "68000090"
+  },
+  {
+    name: "觀音區",
+    locationId: "68000100"
+  },
+  {
+    name: "新屋區",
+    locationId: "68000110"
+  },
+  {
+    name: "龍潭區",
+    locationId: "68000120"
+  },
+  {
+    name: "復興區",
+    locationId: "68000130"
+  }
+];
+
+export const TAOYUAN_DISTRICT_NAMES =
+  TAOYUAN_DISTRICTS.map(x => x.name);
