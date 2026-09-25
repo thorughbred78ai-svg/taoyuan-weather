b63d4a7230921daa7e122b2b55f29cfa44b8c07b
thorughbred78ai-svg <thorughbred78ai@gmail.com>
2026-09-25 09:19:30 +0800

    Update districts.js

--- src/districts.js
+++ src/districts.js
@@ -1,57 +1,63 @@
+// ============================================================
+// 桃園市 13 個行政區
+// ============================================================
+
 export const TAOYUAN_DISTRICTS = [
   {
     name: "桃園區",
-    locationId: "68000010"
+    locationId: "F-D0047-005"
   },
   {
     name: "中壢區",
-    locationId: "68000020"
-  },
-  {
-    name: "平鎮區",
-    locationId: "68000030"
-  },
-  {
-    name: "八德區",
-    locationId: "68000040"
-  },
-  {
-    name: "楊梅區",
-    locationId: "68000050"
-  },
-  {
-    name: "蘆竹區",
-    locationId: "68000060"
+    locationId: "F-D0047-005"
   },
   {
     name: "大溪區",
-    locationId: "68000070"
+    locationId: "F-D0047-005"
   },
   {
-    name: "龜山區",
-    locationId: "68000080"
+    name: "楊梅區",
+    locationId: "F-D0047-005"
+  },
+  {
+    name: "蘆竹區",
+    locationId: "F-D0047-005"
   },
   {
     name: "大園區",
-    locationId: "68000090"
+    locationId: "F-D0047-005"
   },
   {
-    name: "觀音區",
-    locationId: "68000100"
+    name: "龜山區",
+    locationId: "F-D0047-005"
   },
   {
-    name: "新屋區",
-    locationId: "68000110"
+    name: "八德區",
+    locationId: "F-D0047-005"
   },
   {
     name: "龍潭區",
-    locationId: "68000120"
+    locationId: "F-D0047-005"
+  },
+  {
+    name: "平鎮區",
+    locationId: "F-D0047-005"
+  },
+  {
+    name: "新屋區",
+    locationId: "F-D0047-005"
+  },
+  {
+    name: "觀音區",
+    locationId: "F-D0047-005"
   },
   {
     name: "復興區",
-    locationId: "68000130"
+    locationId: "F-D0047-005"
   }
 ];
 
 export const TAOYUAN_DISTRICT_NAMES =
-  TAOYUAN_DISTRICTS.map(x => x.name);
+  TAOYUAN_DISTRICTS.map(
+    district => district.name
+  );
